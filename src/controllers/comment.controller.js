5d0ed23c9d74dd6af70e0fcd0fe4d8a0ab360a07
Majedul Islam <majedulislam956@gmail.com>
2024-11-17 21:10:07 +0000

    like comment controller complete

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,26 +1,107 @@
 import mongoose from "mongoose"
 import {Comment} from "../models/comment.model.js"
+import {Video} from "../models/video.model.js"
+import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const { videoId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
 
+    try {
+        if (!mongoose.Types.ObjectId.isValid(videoId)) {
+            throw new Error("Invalid video ID format");
+        }
+        const comments = await Comment.aggregate([
+            {
+                $match: { video: new mongoose.Types.ObjectId(videoId) }
+            },
+            {
+                $sort: { createdAt: -1 } // Sort by createdAt in descending order
+            },
+            {
+                $skip: (page - 1) * limit // Skip documents for pagination
+            },
+            {
+                $limit: parseInt(limit) // Limit number of documents returned
+            }
+        ]);
+
+        const totalComments = await Comment.countDocuments({ video: videoId });
+
+        res.status(200).json({
+            success: true,
+            total: totalComments,
+            page,
+            pages: Math.ceil(totalComments / limit),
+            comments
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+    const { content } = req.body;
+    const userId = req.user._id
+    const { videoId } = req.params
+
+    const user = await User.findById(userId)
+    if (!user) {
+        throw new ApiError(404, "user not found")
+    }
+
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "video not found")
+    }
+
+    const comment = new Comment({
+        content,
+        video: video._id,
+        owner: userId
+    })
+    
+    await comment.save()
+    res.status(201).json(new ApiResponse(200, comment, "comment created successfully"))
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
+    const { content } = req.body
+    const { commentId } = req.params
+    
+    const comment = await Comment.findById(commentId)
+
+    if (req.user._id.toString() !== comment.owner.toString()) {
+        throw new ApiError(401, "you are unauthorized to modify the comment")
+    }
+
+    if (!comment) {
+        throw new ApiError(404, "comment not found")
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {$set: {content}},
+        {new: true}
+    )
+
+    res.status(200).json(new ApiResponse(200, updatedComment, "comment updated successfully"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    const commentId = req.params.commentId
+
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+        throw new ApiError(404, "comment not found")
+    }
+
+    await Comment.findByIdAndDelete(commentId)
+
+    res.status(200).json(new ApiResponse(200, "Comment deleted"))
 })
 
 export {
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,16 +3,61 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
+import { Comment } from "../models/comment.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
+    const { videoId } = req.params
+
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "video not found")
+    }
+    
+    const existingLike = await Like.findOne({
+        video: videoId,
+        likedBy: req.user._id
+    })
+
+    if (existingLike) {
+        await Like.findByIdAndDelete(existingLike._id)
+        res.status(200).json(new ApiResponse(200, "disliked successfully"))
+    } else {
+        const like = new Like({
+            video: videoId,
+            likedBy: req.user._id
+        })
+        await like.save()
+        res.status(201).json(new ApiResponse(200, like, "liked successfully"))
+    }
+    
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    const comment = await Comment.findById(commentId)
+    if (!comment) {
+        throw new ApiError(404, "comment not found")
+    }
 
+    const existingComment = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user._id
+    })
+
+    if (existingComment) {
+        await Like.findByIdAndDelete(existingComment._id);
+        res.status(200).json(new ApiResponse(200, "Disliked comment"))
+    } else {
+        const like = new Like({
+            comment: comment._id,
+            likedBy: req.user._id
+        })
+        await like.save()
+
+        res.status(201).json(new ApiResponse(200, like, "Liked the comment"))
+    }
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
@@ -22,7 +67,46 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    const userId = req.user._id;
+    try {
+        if (!isValidObjectId(new mongoose.Types.ObjectId(userId))) {
+            throw new ApiError(400, "Invalid user Id")
+        }
+
+        const allLikedVideos = await Like.aggregate([
+            {
+                $match: { likedBy: new mongoose.Types.ObjectId(userId) }
+            },
+            {
+                $lookup: {
+                    from: "videos",
+                    localField: "video",
+                    foreignField: "_id",
+                    as: "videoDetails"
+                }
+            },
+            {
+                $unwind: "$videoDetails"
+            },
+            {
+                $project: {
+                    _id: 1,
+                    videoId: "$videoDetails._id",
+                    videoFile: "$videoDetails.videoFile",
+                    thumbnail: "$videoDetails.thumbnail",
+                    title: "$videoDetails.title"
+                }
+            },
+        ])
+
+        res.status(200).json({
+            allLikedVideos,
+            total: allLikedVideos.length,
+            message: "All liked videos by the logged user"
+        })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 })
 
 export {
